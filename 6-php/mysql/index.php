909179734b707cbdc76af7eff22584a291f6b052
Carlos  A.Massavanhane Jr <cmassavanhanejr@gmail.com>
2021-05-24 12:00:00 +0200

    update 6.3 retrieving data from db

--- 6-php/mysql/index.php
+++ 6-php/mysql/index.php
@@ -1,1 +1,15 @@
-Hello world!
+<?php
+    $link = mysqli_connect("localhost", "root", "Carlos@310319#", "webcourse");
+    if(mysqli_connect_error()){
+        die ("there was an error connecting to the DB");
+    }
+    
+    $query = "SELECT * FROM users";
+
+    if ($result = mysqli_query($link, $query)){
+        echo "Query was successful <br>";
+        $row = mysqli_fetch_array($result);
+        echo "<b>your email is </b>".$row[1]."; <br> <b>Your password is </b>".$row[2];
+        
+    }
+?>
