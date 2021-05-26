75ef40a8782cb6c38a612f430492fc0d24c0aa99
Carlos  A.Massavanhane Jr <cmassavanhanejr@gmail.com>
2021-05-26 15:14:33 +0200

    init(): mysql session

--- 6-php/mysql/index.php
+++ 6-php/mysql/index.php
@@ -1,15 +1,27 @@
 <?php
-    $link = mysqli_connect("localhost", "root", "Carlos@310319#", "webcourse");
+    
+    $host = "localhost";
+    $username  = "root";
+    $password = "Carlos@310319#";
+    $dbname = "webcourse";
+
+   //Creating a connection
+   $link = mysqli_connect($host, $username, $password, $dbname);
     if(mysqli_connect_error()){
         die ("there was an error connecting to the DB");
     }
     
+    //$query = "INSERT INTO `users` (`email`,`password`) VALUES ('carlos@luasiga.com', 'Password');";
+    $query = "UPDATE `users` SET email='camjr@camjr.com' WHERE id = 1 LIMIT 1"
+    mysqli_query($link, $query);
+    
     $query = "SELECT * FROM users";
+
 
     if ($result = mysqli_query($link, $query)){
         echo "Query was successful <br>";
         $row = mysqli_fetch_array($result);
-        echo "<b>your email is </b>".$row[1]."; <br> <b>Your password is </b>".$row[2];
+        echo "<b> email is </b>".$row[1]."; <br> <b>Your password is </b>".$row[2];
         
     }
 ?>
